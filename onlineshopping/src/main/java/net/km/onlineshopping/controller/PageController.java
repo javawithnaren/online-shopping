ca338749d0d16b72fba3d8d3c9ae7404da83cd77
Narendra Babu <narendrababu.ravuri@gmail.com>
2017-07-25 00:14:36 +0530

    loaded static resources

---
+++ onlineshopping/src/main/java/net/km/onlineshopping/controller/PageController.java
@@ -0,0 +1,28 @@
+package net.km.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = {"/" , "/home" , "/index"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("Greeting", "Welcome to spring web MVC");
+		return  mv;
+	}
+	/*@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable(value="greeting",required=false)String greeting) {
+		
+		if(greeting==null){
+			greeting="Hello There";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("Greeting",greeting);
+		return  mv;
+	}*/
+}
