1da8215e5aff26528375c54975a8167eb7008f1c
Narendra Babu <narendrababu.ravuri@gmail.com>
2017-07-26 00:13:27 +0530

    Added Master page to load content dynamically

--- onlineshopping/src/main/java/net/km/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/km/onlineshopping/controller/PageController.java
@@ -12,7 +12,25 @@ public class PageController {
 	@RequestMapping(value = {"/" , "/home" , "/index"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("Greeting", "Welcome to spring web MVC");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
+		return  mv;
+	}
+	
+	
+	@RequestMapping(value ="/about")
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About us");
+		mv.addObject("userClickAbout", true);
+		return  mv;
+	}
+	
+	@RequestMapping(value ="/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
 		return  mv;
 	}
 	/*@RequestMapping(value="/test/{greeting}")
